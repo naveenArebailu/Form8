5503922b0453a41e09b7b703f67ceaff0bba4e8a
naveen arebailu <naveen.arebail@gmail.com>
2017-09-18 22:30:34 +0530

    files added

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { DropDownService } from "app/service/drop-down.service";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule
+  ],
+  providers: [DropDownService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
